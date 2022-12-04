e23e58bd005401ed2d535a941fd7eb66f35eed2b
Costas Akrivoulis <gr33kdude@gmail.com>
2022-12-04 01:44:04 -0800

    Finished Day 4, both parts

--- 2022/4/src/main.rs
+++ 2022/4/src/main.rs
@@ -3,7 +3,15 @@
 use std::env;
 use std::fmt;
 use std::fs;
+use std::ops;
 use std::process;
+
+extern crate interval;
+extern crate gcollections;
+
+use crate::interval::Interval;
+use crate::interval::ops::*;
+use gcollections::ops::*;
 
 fn usage(args: &Vec<String>) {
     println!("Usage: {} <input.txt>", args[0]);
@@ -20,8 +28,33 @@ fn main() {
     let input = fs::read_to_string(filename)
         .expect("Failed to read file");
 
-    let part1 = 0;
-    let part2 = 0;
+    let mut part1 = 0;
+    let mut part2 = 0;
+
+    for line in input.lines() {
+        let mut assignments = line.split(",");
+        let first = assignments.nth(0).unwrap();
+        let mut first = first.split("-");
+        let first = Interval::new(
+            first.nth(0).unwrap().parse::<i32>().unwrap(),
+            first.nth(0).unwrap().parse::<i32>().unwrap()
+        );
+        
+        let second = assignments.nth(0).unwrap();
+        let mut second = second.split("-");
+        let second = Interval::new(
+            second.nth(0).unwrap().parse::<i32>().unwrap(),
+            second.nth(0).unwrap().parse::<i32>().unwrap()
+        );
+
+        if first.is_subset(&second) || second.is_subset(&first) {
+            part1 += 1;
+        }
+
+        if first.overlap(&second) {
+            part2 += 1;
+        }
+    }
 
     println!("Part 1: {:?}", part1);
     println!("Part 2: {:?}", part2);
